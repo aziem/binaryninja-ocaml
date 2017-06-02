2a2733679bc94286877da969696edf6f890777dc
Aziem Chawdhary <aziem@chawdhary.co.uk>
2017-06-02 14:38:55 +0100

    Add missing platform ml

---
+++ lib/platform.ml
@@ -0,0 +1,46 @@
+open Ctypes
+
+type bn_platform = Ffi_bindings.bn_platform Ctypes.structure Ctypes_static.ptr
+
+
+module B = Ffi_bindings.Bindings(Ffi_generated_types)(Ffi_generated)
+
+let create_platform arch name =
+  B.bn_create_platform arch name
+
+let get_platform_name platform =
+  B.bn_get_platform_name platform 
+
+let get_architecture platform =
+  B.bn_get_platform_architecture platform
+
+let get_platform_list () =
+  let i = allocate size_t (Unsigned.Size_t.of_int 0) in
+  let lst = B.bn_get_platform_list i in
+  let sz = (Unsigned.Size_t.to_int !@i) in
+  let rec loop acc i =
+    match i with
+    | _ when i=sz -> acc 
+    | _ as n ->
+      let plat = !@(lst +@ n) in
+      loop ( plat :: acc) (n+1)
+  in
+  loop [] 0
+
+let get_platform_list_by_arch bn_arch =
+  let i = allocate size_t (Unsigned.Size_t.of_int 0) in
+  let lst = B.bn_get_platform_list_by_architecture bn_arch i in
+  let sz = (Unsigned.Size_t.to_int !@i) in
+  let rec loop acc i =
+    match i with
+    | _ when i=sz -> acc 
+    | _ as n ->
+      let plat = !@(lst +@ n) in
+      loop ( plat :: acc) (n+1)
+  in
+  loop [] 0
+
+ 
+      
+    
+
