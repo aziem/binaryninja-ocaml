e283ff4220f41565c94362cdbd4e25efa0db1e92
Aziem Chawdhary <aziem@chawdhary.co.uk>
2017-06-29 10:46:45 +0100

    small refactor

--- lib/function.ml
+++ lib/function.ml
@@ -2,9 +2,9 @@ open Ctypes
 open Foreign
 
 module B = Ffi_bindings.Bindings(Ffi_generated_types)(Ffi_generated)
+module U = Utils
 open Ffi_bindings
 open B
-
 
 type bn_function = Typedefs.bn_function Ctypes.structure Ctypes_static.ptr
     
@@ -26,6 +26,12 @@ let was_function_auto_discovered f =
 let can_function_return f =
   B.bn_can_function_return f
 
+let has_explicitly_defined_type f =
+  B.bnfunction_has_explicitly_defined_type f
+
+let needs_update f =
+  B.bnis_function_update_needed f 
+
 let get_basic_blocks f =
   let i = allocate size_t (Unsigned.Size_t.of_int 0) in
   let lst = B.bn_get_function_basic_block_list f i in
@@ -38,3 +44,14 @@ let get_basic_blocks f =
       loop (block :: acc) (n+1)
   in
   List.rev (loop [] 0)
+
+let get_basic_block_at_addr f arch addr =
+  B.bnget_function_basic_block_at_address f arch addr |> U.optionify_ptr B.E.bn_basicblock
+
+let mark_recently_used f =
+  B.bnmark_function_as_recently_used f
+
+
+
+
+
